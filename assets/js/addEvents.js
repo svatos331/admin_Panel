ed04d7d397350a6ec029b02d2878c5c3c9386885
svatos331 <54244300+svatos331@users.noreply.github.com>
2021-04-12 09:13:00 +0400

    Admin Panel demo
    Admin Panel demo

---
+++ assets/js/addEvents.js
@@ -0,0 +1,14 @@
+
+
+
+
+document.getElementsByClassName('button')[0].addEventListener('click', function(){
+    document.getElementsByClassName('button')[0].classList.toggle('buttonAddtor')
+
+})
+
+
+
+function sayHi(i) {
+    document.getElementsByClassName('modalWindow_0_Item')[i].classList.toggle('activity_W')
+  }
